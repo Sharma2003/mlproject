faf3efe6a0340a923efed225954777785b107504
Tejash Sharma <tejassharma1820@gmail.com>
2024-05-26 15:32:48 +0530

    Data Transformation done

--- scr/component/data_ingestion.py
+++ scr/component/data_ingestion.py
@@ -6,6 +6,8 @@ from scr.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from scr.component.data_transformations import DataTransformation
+from scr.component.data_transformations import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -50,4 +52,6 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_obj,test_obj = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_obj,test_obj)
